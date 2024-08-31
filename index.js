0496ab640842208cb297405df572325486c7e081
Lindsey Monyepao <lindseymonyepao@gmail.com>
2024-09-01 00:06:29 +0200

    added search engine basic functionality

---
+++ index.js
@@ -0,0 +1,13 @@
+
+//this funtion controls the city name change by adding event listner to the search button
+function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector(".search-form-input");
+    let cityElement = document.querySelector(".weather-app-city");
+    cityElement.innerHTML = searchInput.value;
+  }
+  
+  let searchFormElement = document.querySelector(".search-form");
+  searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+ 
